bc2d739a882cff3b84e265ea8bef2bd1f43ece21
moulina pradhan <pmoulina76@gmail.com>
2020-12-02 23:12:34 +0530

    traversal post, pre and inorder

--- src/tress/Tree.java
+++ src/tress/Tree.java
@@ -6,7 +6,15 @@ public class Tree {
     static Scanner sc= null;
     public static void main(String[] args) {
         sc= new Scanner(System.in);
-        createTree();
+        Node root=createTree();
+        System.out.println("Inorder");
+        inOrder(root);
+        System.out.println();
+        System.out.println("PreOrder");
+        preOrder(root);
+        System.out.println();
+        System.out.println("PostOder");
+        postOrder(root);
     }
 
     static Node createTree(){
@@ -21,7 +29,28 @@ public class Tree {
         root.right=createTree();
         return root;
     }
+static void inOrder(Node root){
+if(root == null) return;
+inOrder(root.left);
+System.out.print(root.data+" ");
+inOrder(root.right);
 }
+
+static void preOrder(Node root){
+    if(root == null) return;
+    System.out.print(root.data+" ");
+    preOrder(root.left);
+    preOrder(root.right);
+    }
+
+    static void postOrder(Node root){
+        if(root == null) return;
+        postOrder(root.left);
+       
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+        }
+    }
 
 class Node{
     Node left, right;
