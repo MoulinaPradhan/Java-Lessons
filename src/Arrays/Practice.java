5cd4c13a7c70d1e66b1a5427bbcf9947c0c021bd
moulina pradhan <pmoulina76@gmail.com>
2020-12-13 21:51:46 +0530

    add practice

---
+++ src/Arrays/Practice.java
@@ -0,0 +1,15 @@
+package src.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Practice {
+public static void main (String[] args) {
+System.out.println(Math.floor(3/2));
+int Arr[] = {5, 8, 10, 13,8,13};
+ Arrays.sort(Arr);
+ System.out.println(Arr.length);
+	}
+}
+    
+
