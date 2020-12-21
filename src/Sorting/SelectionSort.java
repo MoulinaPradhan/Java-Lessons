caa32462c27e02d3a5aeff75519bbada1c04acdf
moulina pradhan <pmoulina76@gmail.com>
2020-12-21 14:08:52 +0530

    Selection sort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package src.Sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int a[]={8,6,3,5,0};
+        int n= a.length;
+        for(int i=0;i<n-1;i++){
+           int minInd=i;
+           for(int j=i;j<n;j++){
+               if(a[j]<a[minInd]){
+                   minInd=j;
+               }
+               
+            }
+            int temp=a[i];
+               a[i]=a[minInd];
+               a[minInd]=temp;
+           }
+        
+       
+        for(int e:a) System.out.println(e);
+
+    }
+}
