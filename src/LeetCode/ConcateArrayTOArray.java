520d5d93c4329b01db1f86336e1fae76a4073140
moulina pradhan <pmoulina76@gmail.com>
2021-01-17 20:46:33 +0530

    leetecode hashmap

---
+++ src/LeetCode/ConcateArrayTOArray.java
@@ -0,0 +1,31 @@
+
+package src.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConcateArrayTOArray {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+   Map<Integer,int[]> map = new HashMap<>();
+    for(int i=0; i< pieces.length; i++){
+map.put(pieces[i][0],pieces[i]);
+    }
+    int i=0;
+    while(i<arr.length){
+        int num=arr[i];
+        if(!map.containsKey(num)){
+            return false;
+        }
+        int [] piece = map.get(num);
+        int j=0;
+        while( j < piece.length){
+            if(piece[j] != arr[i]){
+                return false;
+            }
+            j++;
+            i++;
+        }
+    }
+    return true;
+    } 
+}
