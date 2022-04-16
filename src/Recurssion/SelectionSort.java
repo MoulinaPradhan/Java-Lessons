87cef3a0f606248eef012344386c2610ef19bea5
MoulinaPradhan <pmoulina76@gmail.com>
2022-04-16 20:42:57 +0530

    recursive selection sort

---
+++ src/Recurssion/SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+// In selection sort we take the max element from the 
+// slot and swap it with the last element
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] ={4,3,2,1};
+        selectionSort(arr, arr.length, 0,0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+ 
+    public static void selectionSort(int arr[], int r, int c, int max){
+        if(r ==0) return;
+        if(c<r){
+            if(arr[c] > arr[max]){
+              selectionSort(arr, r, c+1, c); 
+            }else{
+            selectionSort(arr, r, c+1,max);
+            }
+        }else{
+            int temp = arr[max];
+            arr[max]= arr[r-1];
+            arr[r-1] = temp;
+            selectionSort(arr, r-1, 0,0);
+        
+        }
+      
+       
+
+
+
+    }
+}
