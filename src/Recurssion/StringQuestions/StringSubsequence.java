864e88efce9f2b4edb1fb7d030cbfb21e7d912e0
MoulinaPradhan <pmoulina76@gmail.com>
2022-04-19 09:37:19 +0530

    Iterative: remove duplicate from subsequence

--- src/Recurssion/StringQuestions/StringSubsequence.java
+++ src/Recurssion/StringQuestions/StringSubsequence.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class StringSubsequence {
     public static void main(String[] args) {
         // st("","abc");
-        int arr[] ={1,2,3};
+        int arr[] ={1,2,2};
         System.out.println(subset(arr));
+        System.out.println(subsetDuplicate(arr));
     }
     static void st(String ans, String og){
         if(og.isEmpty()) {
@@ -26,6 +27,22 @@ public class StringSubsequence {
             for(int i=0;i<n;i++){
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;    
+        
+    }
+// remove Duplicates
+    static List<List<Integer>> subsetDuplicate(int arr[]){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num: arr){
+            int n = outer.size();
+            for(int i=0;i<n;i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                if(!outer.contains(internal))
                 outer.add(internal);
             }
         }
