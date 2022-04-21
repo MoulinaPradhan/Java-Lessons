835e01a61c1c9a4e27084d14a199abf61c5f1533
MoulinaPradhan <pmoulina76@gmail.com>
2022-04-21 11:14:41 +0530

    recursive is Prime

--- src/Recurssion/findPrime.java
+++ src/Recurssion/findPrime.java
@@ -2,8 +2,16 @@ public class findPrime {
     public static void main(String[] args) {
         int n = 11;
         for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i ==0) System.out.println(true);
+            if(n%i ==0) System.out.println(false);
         }
-        System.out.println(false);
+        System.out.println(true);
+
+        System.out.println(prime(2, 49));
+    }
+    static boolean prime(int i, int n){
+        if(i == n)return true;
+        if(n %i ==0) return false;
+      
+      return  prime(i+1, n);
     }
 }
