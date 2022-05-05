239ffd35632dbd0e5aaa6c1d2a9359057d269a53
MoulinaPradhan <pmoulina76@gmail.com>
2022-05-05 21:18:53 +0530

    filter all number greater than 50

--- src/Oops/FilterAll.java
+++ src/Oops/FilterAll.java
@@ -8,12 +8,16 @@ public class FilterAll{
 public static void main(String[] args) {
     List<Integer> li1 = List.of(2,4,5,6,7);
     System.out.println(li1);
-    List<Integer> li2 = Arrays.asList(23,6,7,4);
+    List<Integer> li2 = Arrays.asList(23,6,7,4, 90,45,78,89);
     System.out.println(li2);
 
     // filter all even numbers from li1
     List<Integer>h1 =  li1.stream().filter(i->i%2 == 0).collect(Collectors.toList());
     System.out.println(h1);
+
+    // filter all number greater than 50
+    List<Integer> h2 = li2.stream().filter(i-> i>50).collect(Collectors.toList());
+    System.out.println(h2);
 
 
 }
