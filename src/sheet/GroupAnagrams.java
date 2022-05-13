347b03d3d2facaa116b05aeaf6cac647941da40a
MoulinaPradhan <pmoulina76@gmail.com>
2022-05-13 16:56:17 +0530

    Group Anagrams

---
+++ src/sheet/GroupAnagrams.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/group-anagrams/
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>>map = new HashMap<>();
+        for(String s: strs){
+            char ch[] = s.toCharArray();
+            Arrays.sort(ch);
+            String key = String.valueOf(ch);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+                 map.get(key).add(s);
+            
+        }
+        return new ArrayList<>(map.values());
+    }
+}
