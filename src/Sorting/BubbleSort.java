430ae5cdca558ce72eddc80271c85a07c4ffd3af
Moulina Pradhan <pmoulina76@gmail.com>
2023-03-26 09:50:12 +0530

    Update BubbleSort.java

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -1,21 +1,22 @@
 package src.Sorting;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int arr[]={5,2,3,1};
-       
-        for(int i=0;i<arr.length;i++){
-            for(int j=1;j<arr.length-i;j++){
-                if(arr[j+1]<arr[j]){
-                    int temp=arr[j+1];
-                    arr[j+1]=arr[j];
-                  arr[j]=temp;
-                 
-                }
-            }
+    public static void main (String args[]){
+int arr[] = {5,2,3,1};
+int n = arr.length;
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-1-i;j++){
+        if(arr[j] > arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
         }
-       for(int item :arr){
-           System.out.print(item+ " ");
-       }
     }
 }
+
+System.out.println(Arrays.toString(arr));
+
+    }
+}
+  
