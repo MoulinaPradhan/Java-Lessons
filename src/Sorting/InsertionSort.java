9311f0333e8ead9371342b9104c489a88db333d6
Moulina Pradhan <pmoulina76@gmail.com>
2023-03-26 10:33:26 +0530

    Update InsertionSort.java

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,21 +1,23 @@
-package src.Sorting;
+import java.util.Arrays;
+// In insertion sort we take a key and we want to find its position in sorted part of the arr
 
-public class InsertionSort {
-    public static void main(String[] args) {
-        int arr[]={5,7,2};
-        int n=arr.length;
-        for(int i=0;i<=n-1;i++){
-          for(int j=i-1;j>=0;j--){
-            if(arr[j+1]<arr[j]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-              arr[j]=temp;
-             
-            }
-              }
-        }
-        for(int e :arr){
-            System.out.println(e);
-        }
+public class InsertionSort{
+    public static void main (String args[]){
+int arr[] = {5,2,3,1};
+int n = arr.length;
+
+for(int i=1;i<n;i++){
+    int key = arr[i];
+   int j =i-1;
+   while(j>=0 && arr[j] > key){
+    arr[j+1] =arr[j];
+    j--;
+   }
+   arr[j+1] = key;
+    }
+
+
+System.out.println(Arrays.toString(arr));
+
     }
 }
